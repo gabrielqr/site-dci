79a2cb75fa1697f3460ce25466e6e6be2c47d3d4
gabrielqr <gabrielqr2010@hotmail.com>
2023-07-27 03:17:38 -0300

    adicionando "post" no admin

--- landing/urls.py
+++ landing/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import HomeView, DetalhesNoticiasView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+#    path('', views.index, name='index'),
+    path('', HomeView.as_view(), name="index"),
+    path('noticias/<int:pk>', DetalhesNoticiasView.as_view(), name="detalhes_noticias")
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def index(request):
-    return render(request, 'index.html', {})
+# def index(request):
+#     return render(request, 'index.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'index.html'
+    
+class DetalhesNoticiasView(DetailView):
+    model = Post
+    template_name = 'detalhes_noticias.html'
