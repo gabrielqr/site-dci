54537e3b5095da6e170f0fc171edb9fc9859477b
gabrielqr <gabrielqr2010@hotmail.com>
2023-07-29 19:25:11 -0300

    adding pillow on requirements

--- landing/models.py
+++ landing/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+    image = models.ImageField(null=True, blank= True, upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
