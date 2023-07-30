fa49f3fc77a3095c948932fd90ae252c31eaab40
gabrielqr <gabrielqr2010@hotmail.com>
2023-07-30 02:22:43 -0300

    posts ordenados por data de criação funcionando

--- landing/models.py
+++ landing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
