19a2d5a4657434362f00f568ac1da66bd9380a3e
gabrielqr <gabrielqr2010@hotmail.com>
2023-08-18 02:56:58 -0300

    adicionando navlink no adminpanel

--- landing/admin.py
+++ landing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, MensagemContato, Carrossel
+from .models import Post, MensagemContato, Carrossel, Navlink
 
 admin.site.register(Post)
 admin.site.register(MensagemContato)
 admin.site.register(Carrossel)
+admin.site.register(Navlink)
--- landing/models.py
+++ landing/models.py
@@ -36,3 +36,10 @@ class Carrossel(models.Model):
     
     def __str__(self):
         return self.title
+
+class Navlink(models.Model):
+    title = models.CharField(max_length=30, blank=False)
+    link = models.URLField(blank=False)
+    
+    def __str__(self):
+        return self.title
